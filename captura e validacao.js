46b899759e4bff5846727b08d3b9e09fd937252e
SMAURAI <smaurai1547@gmail.com>
2022-12-18 09:54:11 -0300

    Update captura e validacao.js

--- captura e validacao.js
+++ captura e validacao.js
@@ -1,4 +1,8 @@
-//Javascript
+//DESCRIÇÃO
+/*
+A função captura os valores que foram apresentados nos campos do HTML e valida se foram preenchidos.
+A ideia é que ele realize a validação antes de submeter ao PHP, além disso, aprender como manipular dados que não foram apresentados em um formulário.
+*/
 function main(){
         // Captura dos valores que foram preenchidos no documento HTML
 				var user = document.getElementById("user").value
